f4874189973000ae75d71452df1b440d172b32b4
Antoine LUONG <antoine.luong@c-s.fr>
2019-01-17 16:18:22 +0100

    Adapt to Prewikka 5.0

--- helloworld/__init__.py
+++ helloworld/__init__.py
@@ -1,17 +1,17 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-from prewikka import version, view, template
+from prewikka import template, view
 
 """A HelloWorld plugin"""
 
 class HelloWorld(view.View):
     plugin_name = "Hello World"
     plugin_author = "Antoine Luong"
-    plugin_license = version.__license__
-    plugin_version = version.__version__
-    plugin_copyright = version.__copyright__
+    plugin_license = "GPL"
+    plugin_version = "5.0.0"
+    plugin_copyright = "CSSI"
     plugin_description = N_("A plugin that says hello world!")
 
-    @view.route("/helloworld", menu=(N_('HelloWorld'), N_('HelloWorld')))
+    @view.route("/helloworld", menu=("HelloWorld", "HelloWorld"))
     def render(self):
         return template.PrewikkaTemplate(__name__, "templates/helloworld.mak").render()
